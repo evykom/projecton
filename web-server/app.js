62ea02f419efcc6927bb16aa78aaee06f6f9dfd7
Evyatar Komarovsky <evyatar.komarovsky@live.biu.ac.il>
2026-01-28 14:42:25 +0200

    simplified config

--- web-server/app.js
+++ web-server/app.js
@@ -1,18 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors');
-require('custom-env').env(process.env.NODE_ENV || 'local', './config');
+const cors = require('cors')
+require('custom-env').env('local', './config')
+const app = express()
 
 mongoose.connect(process.env.CONNECTION_STRING)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
+app.use(cors())
+app.use(express.json())
+app.use('/articles', require('./routes/article'))
 
-const app = express();
-app.use(cors(), express.json(), express.urlencoded({ extended: true }));
-app.use('/articles', require('./routes/article'));
-
-const PORT = process.env.PORT || 30000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+app.listen(process.env.PORT || 30000)
